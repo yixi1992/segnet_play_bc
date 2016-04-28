55e8158255a18577e0674bd7173d5e061042e285
yixi1992 <yixi1992@gmail.com>
2016-04-27 23:19:33 -0400

    segnet f1

---
+++ segnetf1/train.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH -t 0:30:00
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --mail-user=yixi@cs.umd.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --job-name="segnetf1"
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpu
+#SBATCH --mem=8000
+
+~/work/yixi/software/caffe-segnet/build/tools/caffe train -gpu 0 -solver segnet_basic_solver.prototxt -weights basic_camvid_surg.caffemodel
+
